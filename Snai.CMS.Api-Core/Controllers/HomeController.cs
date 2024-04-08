01c1bd4bac32ce6967e7b71a6cf173a21ec265a0
alan <xiaolin-liu@qq.com>
2024-04-08 20:11:47 +0800

    Serilog

---
+++ Snai.CMS.Api-Core/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Snai.CMS.Api_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController > _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var value = "hello";
+            _logger.LogInformation($"輸入的資料：{value}");
+            return Ok(new { Id = 1, Value = $"You say {value}" });
+        }
+    }
+}
